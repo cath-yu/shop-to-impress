dcc8b9b55f68432b1a2a480ab95cd380308e1202
catherineyu0611 <yu359@mcmaster.ca>
2026-01-18 03:00:44 -0500

    Singleplayer

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,23 +1,27 @@
 import "./ProductCard.css";
 
 import Heart from '../assets/heart.svg'
+import Dislike from '../assets/dislike.svg';
 
-export default function ProductCard({name, price, store, image, side, onClick}) {
+export default function ProductCard({name, price, store, image, onDislike, onLike}) {
     return (
         <div className="product-card">
             <img className="product-card-image" src={image}></img>
             
             <div className="product-card-text">
                 <h3>{name}</h3>
-                <h3>${price}</h3>
+                {price && <h3>${price}</h3>}
             </div>
 
             <div className="product-card-store">
-                <h4>{store}</h4>
+                <a href={store} target="_blank" rel="noreferrer">
+                    View product
+                </a>
             </div>
 
-            <div className={`product-card-heart-${side}`}>
-                <img className="product-card-heart" src={Heart} onClick={() => onClick(side)}></img>
+            <div className="product-card-like">
+                <img className="product-card-dislike" src={Dislike} onClick={() => onDislike()}></img>
+                <img className="product-card-heart" src={Heart} onClick={() => onLike()}></img>
             </div>
         </div>
     )
